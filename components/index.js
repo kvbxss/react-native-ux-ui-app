921a3bf57f3dd20735f2760a8cc59937520a5c44
kvbxss <kubadownar444@gmail.com>
2023-04-26 17:21:51 +0200

    export subinfo

--- components/index.js
+++ components/index.js
@@ -4,5 +4,6 @@ import FocusedStatusBar from "./FocusedStatusBar";
 import { CircleButton, RectButton } from "./Button";
 import DetailsDesc from "./DetailsDesc";
 import DetailsBid from "./DetailsBid";
+import { SubInfo } from "./SubInfo";
 
-export { HomeHeader, NFTCard, FocusedStatusBar, CircleButton, DetailsBid, DetailsDesc, RectButton };
+export { HomeHeader, NFTCard, SubInfo, FocusedStatusBar, CircleButton, DetailsBid, DetailsDesc, RectButton };
