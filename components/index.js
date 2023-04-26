5d25721cadbd44fadc7034120cd445e9a8d0aca7
kvbxss <kubadownar444@gmail.com>
2023-04-26 16:52:24 +0200

    components exports

---
+++ components/index.js
@@ -0,0 +1,8 @@
+import HomeHeader from "./HomeHeader";
+import NFTCard from "./NFTCard";
+import FocusedStatusBar from "./FocusedStatusBar";
+import { CircleButton, RectButton } from "./Button";
+import DetailsDesc from "./DetailsDesc";
+import DetailsBid from "./DetailsBid";
+
+export { HomeHeader, NFTCard, FocusedStatusBar, CircleButton, DetailsBid, DetailsDesc, RectButton };
